77d6114f81fba67340ff0a818f5c5e37085ac9c6
Sajib Debnath <debnathsajib2024@gmail.com>
2023-03-11 15:14:02 +0530

    4th commit

---
+++ arrays/class-3 2D arrays/Col_Wise-Sum.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int rows, cols;
+    cin >> rows >> cols;
+
+    int arr[rows][cols] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}};
+
+    int sum = 0;
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            sum = sum + arr[j][i];
+        }
+
+        cout << sum << endl;
+        sum = 0;
+    }
+
+    return 0;
+}
